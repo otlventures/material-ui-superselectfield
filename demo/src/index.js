609dfa1b647acf4047b80a8470201db1cddf1805
MaimilianPichler <maximilian.pichler97@gmail.com>
2017-11-06 16:04:48 +0100

    Move project to nwb and split SuperSelectField.js into smaller files

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,33 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React from 'react'
+import { render } from 'react-dom'
+import Router from 'react-router-dom/BrowserRouter'
+import { blueGrey500, blueGrey700, lightBlue500, deepOrange400 } from 'material-ui/styles/colors'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import App from './App'
+import './index.css'
 
-import Example from '../../src'
-
-class Demo extends Component {
-  render() {
-    return <div>
-      <h1>material-ui-superselectfield Demo</h1>
-      <Example/>
-    </div>
+const customTheme = getMuiTheme({
+  fontFamily: 'Raleway, sans-serif',
+  palette: {
+    primary1Color: lightBlue500,
+    accent1Color: deepOrange400,
+    textColor: blueGrey700
+  },
+  checkbox: {
+    checkedColor: deepOrange400,
+    boxColor: blueGrey500
+  },
+  svgIcon: {
+    color: blueGrey500
   }
-}
+})
 
-render(<Demo/>, document.querySelector('#demo'))
+render(
+  <MuiThemeProvider muiTheme={customTheme}>
+    <Router>
+      <App />
+    </Router>
+  </MuiThemeProvider>,
+  document.querySelector('#demo')
+)
