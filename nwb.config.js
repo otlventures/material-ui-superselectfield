b50f0a16b75b31e3fa9c5d83b98cdbac599e7b9e
Raphaël Morineau <raphael.morineau@gmail.com>
2017-11-19 15:29:17 +0100

    Added configuration into babel to include IE11 in transpilation scope. Added missing class-properties transform needed in CodeExamples

--- nwb.config.js
+++ nwb.config.js
@@ -6,6 +6,20 @@ module.exports = {
     esModules: true,
     umd: false
   },
+  babel: {
+    plugins: 'transform-class-properties',
+    presets: [
+      [
+        'env',
+        {
+          targets: {
+            browsers: [ '> 3%' ]
+          },
+          debug: true
+        }
+      ]
+    ]
+  },
   webpack: {
     aliases: {
       'material-ui-superselectfield': path.resolve('src')
